2cbfc8437f58f5a2ae431d7e5902a6623949ff5f
mohit2275 <mohit.chauhan2275@gmail.com>
2023-04-27 16:12:29 +0530

    first commit

---
+++ Asgn1_script.js
@@ -0,0 +1,188 @@
+// Question 4 - +ve or  -ve
+// let x = 90;
+// if (x > 0) {
+//     console.log("It is positive");
+// }
+
+// else if (x < 0) {
+//     console.log("It is negative");
+// }
+// else {
+//     console.log("This is Zero");
+// }
+
+
+// Question 5 - Even or Odd
+// let x = 0;
+// if (x % 2 == !0) {
+//     console.log("It is Odd Number");
+// }
+
+// else {
+//     console.log("It is Even Number");
+// }
+
+
+
+
+// Question 6 - Empty string or not
+// let a = "";
+// if (a == "" && a.length == 0) {
+//     console.log('It is an Empty String');
+// } else {
+//     console.log('It is NOT an Empty String');
+// }
+
+
+
+
+//Question 7 - Undefined or not
+// let x;
+// if (x == undefined) {
+//     console.log('It is Undefined');
+// } else {
+//     console.log('It is Defined');
+// }
+
+
+
+//Question 8 - Null or not
+// let x = null;
+// if (x === null) {
+//     console.log('It is Null');
+// } else {
+//     console.log('It is Not Null');
+// }
+
+
+
+// Question 9 ------- NaN
+let x = 6;
+
+if (isNaN(x)) {
+    console.log('The variable is NaN.');
+} else {
+    console.log('The variable is not NaN.');
+}
+
+
+
+// Question 10 - Equal or Not
+// let a;
+// let b;
+// if (a === b) {
+//     console.log('The variables are Equal');
+// } else {
+//     console.log('The variables are not Equal');
+// }
+
+
+
+
+
+// /////////// ====== CODING QUESTIONS ======  //////////
+
+// Ques 1 -  Greater than 5
+// let x = -2;
+// if (x > 5) {
+//     console.log('It is greater than 5');
+// }
+// else {
+//     console.log('It is less than 5');
+// }
+
+
+// Ques 2 - String is Empty
+// let a = "";
+// if (a == "" && a.length == 0) {
+//     console.log('It is an Empty String');
+// } else {
+//     console.log('It is NOT an Empty String');
+// }
+
+
+
+//  Ques 3 - Even or Odd
+// let x = 0;
+// if (x % 2 == !0) {
+//     console.log("It is Odd Number");
+// }
+
+// else {
+//     console.log("It is Even Number");
+// }
+
+
+
+// Ques 4 - Print 1 to 10 Number
+// for (let x = 1; x <= 10; x++) {
+// console.log(x);
+// }
+
+
+
+//Ques 5 - Print Even no. b/w 1-10
+// let x;
+// for (x = 1; x <= 10; x++) {
+//     if (x % 2 == 0) {
+//         console.log(x);
+//     }
+// }
+
+
+
+//Ques 6 - Print Odd no. b/w 1-10
+// let x;
+// for (x = 1; x <= 10; x++) {
+//     if (x % 2 == !0) {
+//         console.log(x);
+//     }
+// }
+
+
+
+// Ques 7 -   Function for the sum of two numbers
+// function sum(a, b) {
+//     return a + b
+// }
+// console.log(sum(10, 15));
+
+
+// Ques 8 - Fun that returns the length of the string
+// function length() {
+//     return x.length
+// }
+// let x = 'Hello World';
+// console.log(length(x));
+
+
+
+// Ques 9 - Length of Array
+// function length(x) {
+//     return x.length
+// }
+// let arr = [1, 2, 3, 4, 5, 6];
+// console.log(length(arr));
+
+
+
+// Ques 10 - Length of Object
+// function abc(x) {
+//     return Object.keys(x).length
+// }
+// let obj = {
+//     Name: 'Mohit',
+//     age: 26,
+//     Email: 'mohit@gmail.com',
+//     key1: 'Value1',
+//     key2: 'Value2',
+//     key3: 'Value3',
+// }
+// console.log(abc(obj));
+
+
+
+
+
+
+
